b11ec8f10de89ff6bf8db4331834b8092db9004f
FARDIN98 <fardinahmed66@gmail.com>
2022-12-25 23:02:40 +0600

    new commit

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+    const DB_HOST = "localhost";
+    const DB_USER = "root";
+    const DB_PASS = "";
+    const DB_NAME = "students_hub";
+    const SITE_DOMAIN = 'http://localhost';
+    const SITE_DIR = '/students-hub';
+
+    require_once 'pages.php';
---
+++ template/nav-main.php
@@ -0,0 +1,5 @@
+<nav class="nav">
+    <div class="nav-logo">
+        
+        <span class="nav-logo-text">Students-hub</span>
+    </div>
